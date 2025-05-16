3760bde5fcb78ceddfabb12974deadf24792c1a5
Techori <harshrajsharma359@gmail.com>
2025-05-16 15:35:29 +0530

    Update About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -304,7 +304,7 @@ const About = () => {
 
     <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-6">
       {[
-        { name: "Bharat", flag: "aboutus/flag-square-500.png" },
+        { name: "Bharat", flag: "./aboutus/flag-square-500.png" },
         { name: "USA", flag: "🇺🇸" },
         { name: "UK", flag: "🇬🇧" },
         { name: "Canada", flag: "🇨🇦" },
