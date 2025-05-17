905a6e486cd8f7c35ee96293a2e5a80b993fc5b1
Techori <harshrajsharma359@gmail.com>
2025-05-17 18:38:36 +0530

    Update ServiceHighlights.tsx

--- src/components/home/ServiceHighlights.tsx
+++ src/components/home/ServiceHighlights.tsx
@@ -1,4 +1,4 @@
-
+import NoticePopup from '@/components/NoticePopup';
 import React from "react";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Link } from "react-router-dom";
